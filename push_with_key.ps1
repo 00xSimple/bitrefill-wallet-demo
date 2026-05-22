2671f53b2f469eee0275db9d158dc0bf2158ee01
00xSimple <want_simple@foxmail.com>
2026-05-22 16:08:21 +0800

    chore: update

--- push_with_key.ps1
+++ push_with_key.ps1
@@ -2,12 +2,12 @@ $ErrorActionPreference = "Stop"
 
 
 git remote set-url origin git@github.com:00xSimple/bitrefill-wallet-demo.git
-$env:GIT_SSH_COMMAND='ssh -i C:/Users/wyb/.ssh/github_deploy_20260522_153640 -o IdentitiesOnly=yes'
-ssh -i C:/Users/wyb/.ssh/github_deploy_20260522_153640 -T git@github.com
+$env:GIT_SSH_COMMAND='ssh -i C:/Users/wyb/.ssh/github_deploy_nopass -o IdentitiesOnly=yes'
+ssh -i C:/Users/wyb/.ssh/github_deploy_nopass -T git@github.com
 
 # ===== 固定配置（按需修改）=====
 $RepoPath = "C:\Data\code\imtoken\bitrefill-demo"
-$KeyPath = "C:\Users\wyb\.ssh\github_deploy_20260522_153640"
+$KeyPath = "C:/Users/wyb/.ssh/github_deploy_nopass_20260522_160527"
 $CommitMessage = "chore: update"
 $Branch = "master"
 $Remote = "origin"
